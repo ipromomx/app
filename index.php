3af9a7cef9ca8403c4c1d7cbe6d1a5f47a7220dd
Domingo Ruiz <domingoficial1@gmail.com>
2017-10-21 21:47:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $fb = new \Facebook\Facebook([
 $helper = $fb->getRedirectLoginHelper();
 
 $permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('https://app.ipromo.mx/src/fb-callback.php', $permissions);
+$loginUrl = $helper->getLoginUrl('http://app.ipromo.mx/src/fb-callback.php', $permissions);
 /*Facebook*/
 
 
